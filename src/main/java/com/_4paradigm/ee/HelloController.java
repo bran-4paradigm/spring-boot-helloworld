5f95e6487cb5f8d80673bbc139a48e2d35c4e413
wangyiping <wangyiping@4paradigm.com>
2018-06-02 22:16:21 +0800

    first commit with hello

---
+++ src/main/java/com/_4paradigm/ee/HelloController.java
@@ -0,0 +1,14 @@
+package com._4paradigm.ee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello yiping";
+    }
+}
