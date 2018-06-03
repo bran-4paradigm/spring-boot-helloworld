458f41426c985e4952e79c5dbbb9049f4edd1eea
wangyiping <wangyiping@4paradigm.com>
2018-06-03 19:59:25 +0800

    Add PageHelper 4.1.1 to page
    work with mybatis-spring-boot-start 1.3.2

---
+++ src/main/java/com/_4paradigm/ee/config/MyBatisConfiguration.java
@@ -0,0 +1,24 @@
+package com._4paradigm.ee.config;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class MyBatisConfiguration {
+
+    @Bean
+    public PageHelper pageHelper(){
+        System.out.println("MyBatisConfiguration.pageHelper()");
+        PageHelper pageHelper = new PageHelper();
+
+        Properties p = new Properties();
+        p.setProperty("offsetAsPageNum", "true");
+        p.setProperty("rowBoundsWithCount", "true");
+        p.setProperty("reasonable", "true");
+        pageHelper.setProperties(p);
+        return pageHelper;
+    }
+}
--- src/main/java/com/_4paradigm/ee/controller/DemoControlelr.java
+++ src/main/java/com/_4paradigm/ee/controller/DemoControlelr.java
@@ -2,6 +2,7 @@ package com._4paradigm.ee.controller;
 
 import com._4paradigm.ee.Demo;
 import com._4paradigm.ee.service.DemoService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,10 @@ public class DemoControlelr {
 
     @RequestMapping("/likeName")
     public List<Demo> likeName(String name){
+        /*
+        * 第一个参数：第几页，第二个参数：每页的条数
+         */
+        PageHelper.startPage(1, 6);
         return demoService.likeName(name);
     }
 
